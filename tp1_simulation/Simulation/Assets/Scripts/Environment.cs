96dfb7c0ddd3af7fc4132f51b35dbe5b4aefdeb2
Maxime Leroy <maxime.leroy.4@ulaval.ca>
2018-10-01 21:10:40 -0400

    cinematique

---
+++ tp1_simulation/Simulation/Assets/Scripts/Environment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour {
+
+    public float gravity;
+    
+    public Vector3 GetGravitation(Vector3 position)
+    {
+        Vector3 gravitation;
+
+        gravitation.x = 0;
+        gravitation.y = this.gravity;
+        gravitation.z = 0;
+
+        return gravitation;
+    }
+}
